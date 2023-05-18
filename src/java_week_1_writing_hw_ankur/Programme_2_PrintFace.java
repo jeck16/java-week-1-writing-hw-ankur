7a11ae7243c0f2cca26e9859cb7b985c35e07bfc
jeck16 <khushiraval90@gmail.com>
2023-05-18 11:38:00 +0100

    java-week-1-hw-submited

---
+++ src/java_week_1_writing_hw_ankur/Programme_2_PrintFace.java
@@ -0,0 +1,16 @@
+package java_week_1_writing_hw_ankur;
+
+/**
+ * 2.Write s Java program to print a face.
+ */
+
+public class Programme_2_PrintFace {
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '_' | ");
+        System.out.println(" +_____+");
+    }
+
+}
---
+++ src/java_week_1_writing_hw_ankur/Programme_5_Triangle.java
@@ -0,0 +1,23 @@
+package java_week_1_writing_hw_ankur;
+
+/**
+ * Write a Java Program to print below triangle
+ *             *
+ *            * *
+ *           * * *
+ *          * * * *
+ *         * * * * *
+ *        * * * * * *
+ */
+
+public class Programme_5_Triangle {  public static void main(String[] args) {
+    System.out.println("       *       ");
+    System.out.println("      * *      ");
+    System.out.println("     * * *     ");
+    System.out.println("    * * * *    ");
+    System.out.println("   * * * * *   ");
+    System.out.println("  * * * * * *  ");
+
+
+}
+}
